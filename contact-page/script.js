733b4b9ed438bdc10648abcf588972a263d07a02
dartabish <dartabish01@gmail.com>
2024-01-15 13:51:44 +0530

    refactor

--- contact-page/script.js
+++ contact-page/script.js
@@ -1,18 +1,41 @@
-const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll('.input');
 
 function focusFunc() {
   let parent = this.parentNode;
-  parent.classList.add("focus");
+  parent.classList.add('focus');
 }
 
 function blurFunc() {
   let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
+  if (this.value == '') {
+    parent.classList.remove('focus');
   }
 }
 
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
+inputs.forEach(input => {
+  input.addEventListener('focus', focusFunc);
+  input.addEventListener('blur', blurFunc);
+});
+
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  let url =
+    'https://wa.me/919797231194?text=' +
+    'You have a message from a visitor' +
+    '%0aName: ' +
+    encodeURIComponent(name) +
+    '%0aPhone: ' +
+    encodeURIComponent(phone) +
+    '%0aEmail: ' +
+    encodeURIComponent(email) +
+    '%0aMessage: ' +
+    encodeURIComponent(message);
+
+  window.open(url, '_blank').focus();
 });
